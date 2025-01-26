db985486d4f66545a64fb3bce3bdd3a699a17014
masazumi0526 <n_zumi529@yahoo.co.jp>
2025-01-26 19:23:28 +0900

    基礎２＿０１〜０３

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
+// 修正箇所: ReactDOM.createRoot を使用してルートを作成
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+// ReactDOM.render(
+root.render(
   <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,21 +2,24 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory, Redirect } from "react-router-dom";
+// import { useHistory, Redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { signIn } from "../authSlice";
 import { Header } from "../components/Header";
 import { url } from "../const";
-import "./signUp.css";
+import "./signUp.scss";
 
 export const SignUp = () => {
-  const history = useHistory();
+  // const history = useHistory();
+  const Navigate = useNavigate();
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessge] = useState();
-  const [cookies, setCookie, removeCookie] = useCookies();
+  // const [cookies, setCookie, removeCookie] = useCookies();
+  const [setCookie] = useCookies();
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handleNameChange = (e) => setName(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
@@ -24,22 +27,25 @@ export const SignUp = () => {
     const data = {
       email: email,
       name: name,
-      password: password
+      password: password,
     };
 
-    axios.post(`${url}/users`, data)
+    axios
+      .post(`${url}/users`, data)
       .then((res) => {
         const token = res.data.token;
         dispatch(signIn());
         setCookie("token", token);
-        history.push("/");
+        // history.push("/");
+        Navigate("/");
       })
       .catch((err) => {
         setErrorMessge(`サインアップに失敗しました。 ${err}`);
-      })
+      });
 
-      if(auth) return <Redirect to="/" />
-  }
+    // if(auth) return <Redirect to="/" />
+    if (auth) return <Navigate to="/" />;
+  };
   return (
     <div>
       <Header />
@@ -47,15 +53,35 @@ export const SignUp = () => {
         <h2>新規作成</h2>
         <p className="error-message">{errorMessage}</p>
         <form className="signup-form">
-          <label>メールアドレス</label><br />
-          <input type="email" onChange={handleEmailChange} className="email-input" /><br />
-          <label>ユーザ名</label><br />
-          <input type="text" onChange={handleNameChange} className="name-input" /><br />
-          <label>パスワード</label><br />
-          <input type="password" onChange={handlePasswordChange} className="password-input" /><br />
-          <button type="button" onClick={onSignUp} className="signup-button">作成</button>
+          <label>メールアドレス</label>
+          <br />
+          <input
+            type="email"
+            onChange={handleEmailChange}
+            className="email-input"
+          />
+          <br />
+          <label>ユーザ名</label>
+          <br />
+          <input
+            type="text"
+            onChange={handleNameChange}
+            className="name-input"
+          />
+          <br />
+          <label>パスワード</label>
+          <br />
+          <input
+            type="password"
+            onChange={handlePasswordChange}
+            className="password-input"
+          />
+          <br />
+          <button type="button" onClick={onSignUp} className="signup-button">
+            作成
+          </button>
         </form>
       </main>
     </div>
-  )
-}
+  );
+};
