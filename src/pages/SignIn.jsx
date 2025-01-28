a76e2982652456dd63884db5245936071d390594
masazumi0526 <n_zumi529@yahoo.co.jp>
2025-01-29 01:28:20 +0900

    cookies 修正

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -18,7 +18,7 @@ export const SignIn = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState();
   // const [cookies, setCookie, removeCookie] = useCookies();
-  const [setCookie] = useCookies();
+  const [cookies, setCookie] = useCookies();
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
   const onSignIn = () => {
